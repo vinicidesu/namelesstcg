e0ec099e3c1b0225856f88e60d515110271974cb
Vinicidesu <viniciusmoraeslima@gmail.com>
2025-05-25 03:25:40 -0300

    [fix]: fixing nginx and dockerfile configs

--- frontend/nameless-ui/vite.config.js
+++ frontend/nameless-ui/vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  server: {
+    proxy: {
+      // Redireciona requisições de /api (ou qualquer outro prefixo que você escolher)
+      // para o seu backend. Você pode usar '/api' como um prefixo comum.
+      '/api': {
+        target: 'http://localhost:8001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '') // Remove o /api do path final
+      }
+    }
+  }
 })
